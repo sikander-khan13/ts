f162d9db83a37f13fdf43fbb422d0474f236d8cc
sikander-khan13 <sikander13njj@gmail.com>
2025-11-04 13:06:09 +0600

    Add vite build script and vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import {resolve} from "path";
+import { defineConfig } from "vite";
+export default defineConfig({
+    build:{
+        rollupOptions:{
+            input:{
+                main:resolve(__dirname, "index.html"),
+            },
+        },
+    },
+});
